de4d362dc05df0026eb25876a044189b58637578
MPIREDDU23 <m.pireddu23@studenti.unica.it>
2025-05-24 16:49:57 +0200

    fixed register_event_for_user

--- app/routers/events.py
+++ app/routers/events.py
@@ -45,7 +45,7 @@ def add_event(session: SessionDep, event_data: EventCreate):
     return new_event
 
 @router.post("/{id}/register")
-def register(
+def register_event_for_user(
     request: Request,
     id: Annotated[int, Path(description="ID dell'evento")],
     user: Annotated[str, Path(description="L'username dell'utente")],
@@ -60,10 +60,10 @@ def register(
     user = session.get(User, user.username)
     if user is None:
         raise HTTPException(status_code=404, detail="Utente non trovato")
-    registration_check = session.exec(select(Registration).where((Registration.username == event.username) & (Registration.event_id == id))).first()
+    registration_check = session.exec(select(Registration).where((Registration.username == user.username) & (Registration.event_id == event.id))).first()
     if registration_check:
         raise HTTPException(status_code=409, detail="L'utente è già registrato per l'evento")
-    registration = Registration(username=user.username, event_id=id)
+    registration = Registration(username=user.username, event_id=event.id)
     session.add(registration)
     session.commit()
     return "Utente registrato"
