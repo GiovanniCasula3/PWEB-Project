4d8c8424ef597de3383e9badcd68b1cf4dc81eca
MPIREDDU23 <m.pireddu23@studenti.unica.it>
2025-05-24 17:34:17 +0200

    removed onupdate

--- app/models/registration.py
+++ app/models/registration.py
@@ -1,8 +1,8 @@
 from sqlmodel import SQLModel, Field
 
 class Registration(SQLModel, table=True):
-    username: str = Field(primary_key=True, foreign_key="user.username", ondelete="CASCADE", onupdate="CASCADE")
-    event_id: int = Field(primary_key=True, foreign_key="event.id", ondelete="CASCADE", onupdate="CASCADE")
+    username: str = Field(primary_key=True, foreign_key="user.username", ondelete="CASCADE")
+    event_id: int = Field(primary_key=True, foreign_key="event.id", ondelete="CASCADE")
 
 class RegistrationPublic(Registration):
     """
