284531fd0ce95e387a3e7cc0631b4959ab1501dd
Francesco Carcangiu <f.carcangiu5@studenti.unica.it>
2025-05-24 11:51:51 +0200

    parte 2

--- app/routers/registrations.py
+++ app/routers/registrations.py
@@ -1,5 +1,14 @@
 from fastapi import APIRouter # Serve per definire le rotte API
-
+from sqlmodel import delete, select # Serve per interagire con il database
+from data.db import SessionDep # Importa la dipendenza per ottenere una sessione del database
+from models.registration import Registration # Importa il modello di registrazione pubblica
 
 routers = APIRouter(prefix="/registrations", tags=["registrations"])
+
+
+
+@routers.get("/")
+def get_registrations(
+    session: SessionDep # Dipendenza per ottenere una sessione del database
+) -> list[]
 
