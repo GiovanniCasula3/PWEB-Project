9909496a42c1a50eae733d2d5c59d123ca5b22b9
Francesco Carcangiu <f.carcangiu5@studenti.unica.it>
2025-05-24 11:53:15 +0200

    Aggiunta RegistrationPublic

--- app/models/registration.py
+++ app/models/registration.py
@@ -4,3 +4,11 @@ from sqlmodel import SQLModel, Field
 class Registration(SQLModel, table=True):
     username: str = Field(primary_key=True, foreign_key="user.username")
     event_id: int = Field(primary_key=True, foreign_key="event.id")
+    
+
+class RegistrationPublic(Registration):
+    """
+    Modello utilizzato per restituire tutti i dati delle registrazioni nelle risposte API.
+    Estende Registration e include i campi necessari per la visualizzazione pubblica.
+    """
+    pass
