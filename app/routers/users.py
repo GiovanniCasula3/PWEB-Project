fe3a75d1f81a8fb3a4d90b003ab60ea9ef1d6dfd
MPIREDDU23 <m.pireddu23@studenti.unica.it>
2025-05-24 16:01:07 +0200

    fixed delete all books

--- app/routers/users.py
+++ app/routers/users.py
@@ -56,7 +56,7 @@ def delete_all_users(
 ):
     """ Cancella tutti gli utenti. """
     statement = delete(User)
-    session.exec(statement).one_or_none()
+    session.exec(statement)
     session.commit()
     return "Tutti gli utenti sono stati cancellati."
 
